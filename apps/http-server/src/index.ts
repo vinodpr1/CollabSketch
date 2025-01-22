d27b78ae49752c5a4ff6539e29e07b6b4ddc0eb3
Vinod Prajapati <vinodpr737947@gmail.com>
2025-01-22 18:28:39 +0530

    postgre db configured

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors"
 import bodyParser from "body-parser";
 import { appRoutes } from "./routes";
+import {prismaClient} from "@repo/db/prismaclient";
 
 const app = express();
 app.use(cors());
@@ -12,5 +13,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", appRoutes);
 
 app.listen(3200,async()=>{
+   await prismaClient.user.create({data:{name:"Jay Mata ji", email:"jay@gmail.com", password:"vinod"}});
    console.log(`Server is running on PORT 3200`);
 })
