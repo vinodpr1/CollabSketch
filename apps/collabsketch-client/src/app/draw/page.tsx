ea1689adcabe50f08e64bff1dc1d9e672af35042
Vinod Prajapati <vinodpr737947@gmail.com>
2025-01-23 12:40:24 +0530

    line and circle added

--- apps/collabsketch-client/src/app/draw/page.tsx
+++ apps/collabsketch-client/src/app/draw/page.tsx
@@ -1,25 +1,31 @@
 "use client"
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRef } from 'react'
 import { drawShape } from '../utils/drawshape';
 
 const page = () => {
 
     const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [shape, setShape] = useState<string>("line");
 
     useEffect(()=>{
         if(canvasRef.current){
             const canvas = canvasRef.current;
-            drawShape(canvas);
+            drawShape(canvas, shape);
         }
-    },[canvasRef]);
+    },[canvasRef, shape]);
 
   return (
     <div>
+         <div className='p-4 flex gap-2'>
+           <button onClick={()=>setShape("rect")} className='px-2 py-1 bg-gray-500 rounded'>Rect</button>
+           <button onClick={()=>setShape("arc")} className='px-2 py-1 bg-gray-500 rounded'>Arc</button>
+           <button onClick={()=>setShape("line")} className='px-2 py-1 bg-gray-500 rounded'>Line</button>
+         </div>
         <canvas
           ref={canvasRef}
-          height={580}
-          width={1280}
+          height={700}
+          width={1380}
           className='rgba(0, 0, 0)'
         />
     </div>
--- apps/collabsketch-client/src/app/utils/drawshape.ts
+++ apps/collabsketch-client/src/app/utils/drawshape.ts
@@ -9,16 +9,22 @@
       startX: number,
       startY: number,
       radius: number,
-      startAngle: number,
-      endAngle: number
+    } | {
+        type:"line",
+        startX: number,
+        startY: number,
+        moveX: number,
+        moveY: number
     };
 
-export const drawShape = (canvas:HTMLCanvasElement) =>{
+const existingShape:ExistingShape[] = [];
+
+export const drawShape = (canvas:HTMLCanvasElement, shape:string) =>{
     const ctx = canvas.getContext("2d");
     if(!ctx) return;
-
-    const existingShape:ExistingShape[] = [];
-
+   
+    console.log("Shape name is", shape);
+  
     ctx.fillStyle= "rgba(0, 0, 0)"
     ctx.fillRect(0,0,canvas.width, canvas.height);
     
@@ -27,50 +33,84 @@ export const drawShape = (canvas:HTMLCanvasElement) =>{
     let height:number=0;
     let width:number=0;
     let clicked: boolean = false;
+   
 
     canvas.addEventListener("mousedown",(event:MouseEvent)=>{
         clicked=true;
-        startX = event.clientX;
-        startY = event.clientY;
+        const rect = canvas.getBoundingClientRect();
+        startX = event.clientX - rect.left;
+        startY = event.clientY - rect.top;
     })
 
     canvas.addEventListener("mouseup",(event:MouseEvent)=>{
         clicked=false;
-        existingShape.push({type:"rect", startX:startX, startY: startY, width: width, height: height});
+        const rect = canvas.getBoundingClientRect();
+        if(shape=="rect"){
+            existingShape.push({type:"rect", startX:startX, startY: startY, width: width, height: height });
+        }else if(shape=="arc"){
+            const radius = Math.sqrt(width ** 2 + height ** 2);
+            existingShape.push({type:"arc", startX:startX, startY: startY, radius: radius });
+        }else if(shape=="line"){
+            existingShape.push({type:"line", startX:startX, startY: startY, moveX: event.clientX-rect.left, moveY: event.clientY-rect.top });
+        }
     })
 
     canvas.addEventListener("mousemove",(event:MouseEvent)=>{
       
       if(clicked){
+        const rect = canvas.getBoundingClientRect();
+        width = event.clientX - startX - rect.left;
+        height = event.clientY - startY - rect.top;
 
-          width = event.clientX - startX;
-          height = event.clientY - startY;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle= "rgba(0, 0, 0)"
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-          ctx.fillStyle= "rgba(0, 0, 0)"
-          ctx.fillRect(0, 0, canvas.width, canvas.height)
+        drawShapesBeforeClear(ctx, canvas, existingShape);
 
-          drawShapesBeforeClear(ctx, canvas, existingShape);
-
-          ctx.strokeStyle= "white";
-          ctx.strokeRect(startX, startY, width, height);
-
+        if(shape=="rect"){
+            ctx.strokeStyle= "white";
+            ctx.strokeRect(startX, startY, width, height);
+        }else if(shape=="arc"){
+            const radius = Math.sqrt(width ** 2 + height ** 2);
+            ctx.beginPath();
+            ctx.strokeStyle= "white";
+            ctx.arc(startX, startY, radius, 0, 2 * Math.PI);
+            ctx.stroke();
+        }else if(shape=="line"){
+            ctx.beginPath();
+            ctx.moveTo( startX, startY );
+            ctx.lineTo( event.clientX-rect.left, event.clientY-rect.top );
+            ctx.strokeStyle= "white";
+            ctx.stroke();
+        }
+        
       }
+   });
+   
 
-    })
 }
 
 
 const drawShapesBeforeClear=(ctx:CanvasRenderingContext2D , canvas:HTMLCanvasElement, existingShape:ExistingShape[])=>{
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle= "rgba(0, 0, 0)"
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
     existingShape.map((shape:ExistingShape)=>{
         if(shape.type == "rect"){
           ctx.strokeStyle= "white";
           ctx.strokeRect(shape.startX, shape.startY, shape.width, shape.height);
         }
+        else if(shape.type == "arc"){
+            ctx.beginPath();
+            ctx.arc(shape.startX, shape.startY, shape.radius , 0, 2 * Math.PI); // Circle centered at (100, 100) with radius 50
+            ctx.strokeStyle= "white";
+            ctx.stroke();
+        }else if(shape.type == "line"){
+            ctx.beginPath();
+            ctx.moveTo( shape.startX, shape.startY );
+            ctx.lineTo( shape.moveX, shape.moveY );
+            ctx.strokeStyle= "white";
+            ctx.stroke();
+        }
     })
 
 }
+
