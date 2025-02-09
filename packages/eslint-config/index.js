e1342b8815bc273d07ed5ece89442acf275095dc
Vinod Prajapati <vinodpr737947@gmail.com>
2025-02-09 17:57:22 +0530

    linting fixed

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -2,14 +2,14 @@ import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
-import onlyWarn from "eslint-plugin-only-warn";
+
 
 /**
  * A shared ESLint configuration for the repository.
  *
  * @type {import("eslint").Linter.Config}
  * */
-export const config = [
+export const ESLintConfig = [
   js.configs.recommended,
   eslintConfigPrettier,
   ...tseslint.configs.recommended,
@@ -19,6 +19,11 @@ export const config = [
     },
     rules: {
       "turbo/no-undeclared-env-vars": "warn",
+      "no-unused-vars": "warn",
+      "require-await": "error",
+      "space-before-blocks": ["error", "always"],
+      "space-in-parens": ["error", "never"],
+      "no-multi-spaces": ["error"],
     },
   },
   {
