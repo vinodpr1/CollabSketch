6e6295e7dd004c1dbebf05cbecd3de4313135984
Vinod Prajapati <vinodpr737947@gmail.com>
2025-02-11 18:55:11 +0530

    resize rectangle added

--- apps/collabsketch-client/src/utils/drawshape.ts
+++ apps/collabsketch-client/src/utils/drawshape.ts
@@ -8,60 +8,60 @@ interface Pencil {
 
 type ExistingShape =
   | {
-      id: number;
-      type: "rectangle";
-      color: string;
-      stroke: number;
-      startX: number;
-      startY: number;
-      width: number;
-      height: number;
-    }
+    id: number;
+    type: "rectangle";
+    color: string;
+    stroke: number;
+    startX: number;
+    startY: number;
+    width: number;
+    height: number;
+  }
   | {
-      id: number;
-      type: "ellipse";
-      color: string;
-      stroke: number;
-      startX: number;
-      startY: number;
-      radius: number;
-    }
+    id: number;
+    type: "ellipse";
+    color: string;
+    stroke: number;
+    startX: number;
+    startY: number;
+    radius: number;
+  }
   | {
-      id: number;
-      type: "line";
-      color: string;
-      stroke: number;
-      startX: number;
-      startY: number;
-      moveX: number;
-      moveY: number;
-    }
+    id: number;
+    type: "line";
+    color: string;
+    stroke: number;
+    startX: number;
+    startY: number;
+    moveX: number;
+    moveY: number;
+  }
   | {
-      id: number;
-      type: "pencil";
-      color: string;
-      stroke: number;
-      path: any;
-    }
+    id: number;
+    type: "pencil";
+    color: string;
+    stroke: number;
+    path: any;
+  }
   | {
-      id: number;
-      type: "arrow";
-      color: string;
-      stroke: number;
-      startX: number;
-      startY: number;
-      moveX: number;
-      moveY: number;
-    }
+    id: number;
+    type: "arrow";
+    color: string;
+    stroke: number;
+    startX: number;
+    startY: number;
+    moveX: number;
+    moveY: number;
+  }
   | {
-      id: number;
-      type: "text";
-      color: string;
-      stroke: number;
-      startX: number;
-      startY: number;
-      text: string;
-    };
+    id: number;
+    type: "text";
+    color: string;
+    stroke: number;
+    startX: number;
+    startY: number;
+    text: string;
+  };
 
 let existingShape: ExistingShape[] = [];
 let pencilPath: Pencil[] = [];
@@ -97,6 +97,11 @@ export const drawShape = (
   let slectedX = 0;
   let slectedY = 0;
 
+  let TL = false;
+  let TR = false;
+  let BL = false;
+  let BR = false;
+
   // Store previous event listeners to remove them properly
   const previousListeners = (canvas as any)._eventListeners || {};
 
@@ -113,6 +118,7 @@ export const drawShape = (
     clicked = true;
     const rect = canvas.getBoundingClientRect();
     if (tool == "select") {
+
       selectedShape = existingShape.find((shape) => {
         if (shape.type != "pencil") {
           slecteOffsetX = event.clientX - shape?.startX;
@@ -128,6 +134,73 @@ export const drawShape = (
       if (selectedShape) {
         document.getElementsByTagName("body")[0].style.cursor = "move";
       }
+
+      if (selectedShape?.type == "rectangle") {
+
+        const zoneX1 = selectedShape.startX;
+        const zoneY1 = selectedShape.startY;
+
+        // ctx.beginPath();
+        // ctx.arc(zoneX1, zoneY1, 20, 0, 2 * Math.PI);
+        // ctx.stroke();
+
+        const shape1 = {
+          id: -1,
+          type: "ellipse",
+          color: color,
+          stroke: stroke,
+          startX: zoneX1,
+          startY: zoneY1,
+          radius: 20
+        };
+
+        const shape2 = {
+          id: -1,
+          type: "ellipse",
+          color: color,
+          stroke: stroke,
+          startX: zoneX1 + selectedShape.width,
+          startY: zoneY1 + selectedShape.height,
+          radius: 20
+        };
+
+        const shape3 = {
+          id: -1,
+          type: "ellipse",
+          color: color,
+          stroke: stroke,
+          startX: selectedShape.startX,
+          startY: selectedShape.startY + selectedShape.height,
+          radius: 20
+        };
+
+
+        const shape4 = {
+          id: -1,
+          type: "ellipse",
+          color: color,
+          stroke: stroke,
+          startX: selectedShape.startX + selectedShape.width,
+          startY: selectedShape.startY,
+          radius: 20
+        };
+
+        TL = findInterSection(event.clientX, event.clientY, shape1);
+        BR = findInterSection(event.clientX, event.clientY, shape2);
+        TR = findInterSection(event.clientX, event.clientY, shape3);
+        BL = findInterSection(event.clientX, event.clientY, shape4);
+
+
+        console.log("is inside", TL, TR, BL, BR);
+
+        if (TL || BR) {
+          document.getElementsByTagName("body")[0].style.cursor = "nwse-resize";
+        } else if (TR || BL) {
+          document.getElementsByTagName("body")[0].style.cursor = "nesw-resize";
+        }
+
+      }
+
     } else {
       startX = event.clientX - rect.left;
       startY = event.clientY - rect.top;
@@ -285,108 +358,166 @@ export const drawShape = (
           return shape.id !== selectedShape?.id;
         });
 
-        if (selectedShape.type == "rectangle") {
-          ctx.strokeStyle = selectedShape.color;
-          ctx.lineWidth = selectedShape.stroke;
-          ctx.strokeRect(
-            event.clientX - rect.left - slecteOffsetX,
-            event.clientY - rect.top - slecteOffsetY,
-            selectedShape.width,
-            selectedShape.height,
-          );
-          selectedShape.startX = event.clientX - rect.left - slecteOffsetX;
-          selectedShape.startY = event.clientY - rect.left - slecteOffsetY;
-        } else if (selectedShape.type == "ellipse") {
-          ctx.strokeStyle = selectedShape.color;
-          ctx.lineWidth = selectedShape.stroke;
-          ctx.beginPath();
-          ctx.arc(
-            event.clientX - rect.left - slecteOffsetX,
-            event.clientY - rect.top - slecteOffsetY,
-            selectedShape.radius,
-            0,
-            2 * Math.PI,
-          );
-          ctx.stroke();
-          ctx.closePath();
+        if (!TL && !TR && !BL && !BR) {
+          if (selectedShape.type == "rectangle") {
+            ctx.strokeStyle = selectedShape.color;
+            ctx.lineWidth = selectedShape.stroke;
+            ctx.strokeRect(
+              event.clientX - rect.left - slecteOffsetX,
+              event.clientY - rect.top - slecteOffsetY,
+              selectedShape.width,
+              selectedShape.height,
+            );
+            selectedShape.startX = event.clientX - rect.left - slecteOffsetX;
+            selectedShape.startY = event.clientY - rect.left - slecteOffsetY;
+          } else if (selectedShape.type == "ellipse") {
+            ctx.strokeStyle = selectedShape.color;
+            ctx.lineWidth = selectedShape.stroke;
+            ctx.beginPath();
+            ctx.arc(
+              event.clientX - rect.left - slecteOffsetX,
+              event.clientY - rect.top - slecteOffsetY,
+              selectedShape.radius,
+              0,
+              2 * Math.PI,
+            );
+            ctx.stroke();
+            ctx.closePath();
 
-          selectedShape.startX = event.clientX - rect.left - slecteOffsetX;
-          selectedShape.startY = event.clientY - rect.left - slecteOffsetY;
-        } else if (selectedShape.type == "line") {
-          console.log(
-            "line is selected",
-            selectedShape,
-            event.clientX,
-            event.clientY,
-          );
+            selectedShape.startX = event.clientX - rect.left - slecteOffsetX;
+            selectedShape.startY = event.clientY - rect.left - slecteOffsetY;
+          } else if (selectedShape.type == "line") {
+            console.log(
+              "line is selected",
+              selectedShape,
+              event.clientX,
+              event.clientY,
+            );
 
-          ctx.strokeStyle = selectedShape.color;
-          ctx.lineWidth = selectedShape.stroke;
-          ctx.beginPath();
+            ctx.strokeStyle = selectedShape.color;
+            ctx.lineWidth = selectedShape.stroke;
+            ctx.beginPath();
 
-          // Calculate new start and end positions based on movement
-          let newStartX = event.clientX - rect.left;
-          let newStartY = event.clientY - rect.top;
-          let newEndX =
-            newStartX + (selectedShape.moveX - selectedShape.startX);
-          let newEndY =
-            newStartY + (selectedShape.moveY - selectedShape.startY);
+            // Calculate new start and end positions based on movement
+            let newStartX = event.clientX - rect.left;
+            let newStartY = event.clientY - rect.top;
+            let newEndX =
+              newStartX + (selectedShape.moveX - selectedShape.startX);
+            let newEndY =
+              newStartY + (selectedShape.moveY - selectedShape.startY);
 
-          ctx.moveTo(newStartX - slecteOffsetX, newStartY - slecteOffsetY);
-          ctx.lineTo(newEndX - slecteOffsetX, newEndY - slecteOffsetY);
-          ctx.stroke();
-          ctx.closePath();
+            ctx.moveTo(newStartX - slecteOffsetX, newStartY - slecteOffsetY);
+            ctx.lineTo(newEndX - slecteOffsetX, newEndY - slecteOffsetY);
+            ctx.stroke();
+            ctx.closePath();
 
-          // Update shape's new position
-          selectedShape.startX = newStartX - slecteOffsetX;
-          selectedShape.startY = newStartY - slecteOffsetY;
-          selectedShape.moveX = newEndX - slecteOffsetX;
-          selectedShape.moveY = newEndY - slecteOffsetY;
-        } else if (selectedShape.type == "arrow") {
-          ctx.strokeStyle = selectedShape.color;
-          ctx.lineWidth = selectedShape.stroke;
-          ctx.beginPath();
+            // Update shape's new position
+            selectedShape.startX = newStartX - slecteOffsetX;
+            selectedShape.startY = newStartY - slecteOffsetY;
+            selectedShape.moveX = newEndX - slecteOffsetX;
+            selectedShape.moveY = newEndY - slecteOffsetY;
+          } else if (selectedShape.type == "arrow") {
+            ctx.strokeStyle = selectedShape.color;
+            ctx.lineWidth = selectedShape.stroke;
+            ctx.beginPath();
 
-          // Calculate new start and end positions based on movement
-          let newStartX = event.clientX - rect.left;
-          let newStartY = event.clientY - rect.top;
-          let newEndX =
-            newStartX + (selectedShape.moveX - selectedShape.startX);
-          let newEndY =
-            newStartY + (selectedShape.moveY - selectedShape.startY);
+            // Calculate new start and end positions based on movement
+            let newStartX = event.clientX - rect.left;
+            let newStartY = event.clientY - rect.top;
+            let newEndX =
+              newStartX + (selectedShape.moveX - selectedShape.startX);
+            let newEndY =
+              newStartY + (selectedShape.moveY - selectedShape.startY);
 
-          ctx.beginPath();
-          ctx.moveTo(newStartX - slecteOffsetX, newStartY - slecteOffsetY);
-          ctx.lineTo(newEndX - slecteOffsetX, newEndY - slecteOffsetY);
-          ctx.stroke();
-          ctx.closePath();
+            ctx.beginPath();
+            ctx.moveTo(newStartX - slecteOffsetX, newStartY - slecteOffsetY);
+            ctx.lineTo(newEndX - slecteOffsetX, newEndY - slecteOffsetY);
+            ctx.stroke();
+            ctx.closePath();
 
-          const arrowLen = 10;
-          let dx = newEndX - newStartX;
-          let dy = newEndY - newStartY;
-          let angle = Math.atan2(dy, dx);
+            const arrowLen = 10;
+            let dx = newEndX - newStartX;
+            let dy = newEndY - newStartY;
+            let angle = Math.atan2(dy, dx);
 
-          ctx.moveTo(newEndX - slecteOffsetX, newEndY - slecteOffsetY);
-          ctx.lineTo(
-            newEndX - slecteOffsetX - arrowLen * Math.cos(angle - Math.PI / 6),
-            newEndY - slecteOffsetY - arrowLen * Math.sin(angle - Math.PI / 6),
-          );
-          ctx.stroke();
+            ctx.moveTo(newEndX - slecteOffsetX, newEndY - slecteOffsetY);
+            ctx.lineTo(
+              newEndX - slecteOffsetX - arrowLen * Math.cos(angle - Math.PI / 6),
+              newEndY - slecteOffsetY - arrowLen * Math.sin(angle - Math.PI / 6),
+            );
+            ctx.stroke();
 
-          ctx.moveTo(newEndX - slecteOffsetX, newEndY - slecteOffsetY);
-          ctx.lineTo(
-            newEndX - slecteOffsetX - arrowLen * Math.cos(angle + Math.PI / 6),
-            newEndY - slecteOffsetY - arrowLen * Math.sin(angle + Math.PI / 6),
-          );
+            ctx.moveTo(newEndX - slecteOffsetX, newEndY - slecteOffsetY);
+            ctx.lineTo(
+              newEndX - slecteOffsetX - arrowLen * Math.cos(angle + Math.PI / 6),
+              newEndY - slecteOffsetY - arrowLen * Math.sin(angle + Math.PI / 6),
+            );
 
-          ctx.stroke();
+            ctx.stroke();
 
-          // Update shape's new position
-          selectedShape.startX = newStartX - slecteOffsetX;
-          selectedShape.startY = newStartY - slecteOffsetY;
-          selectedShape.moveX = newEndX - slecteOffsetX;
-          selectedShape.moveY = newEndY - slecteOffsetY;
+            // Update shape's new position
+            selectedShape.startX = newStartX - slecteOffsetX;
+            selectedShape.startY = newStartY - slecteOffsetY;
+            selectedShape.moveX = newEndX - slecteOffsetX;
+            selectedShape.moveY = newEndY - slecteOffsetY;
+          }
+        } else {
+
+          if (selectedShape.type == "rectangle" && TL) {
+            // console.log(selectedShape);
+            ctx.strokeStyle = selectedShape.color;
+            ctx.lineWidth = selectedShape.stroke;
+            const x2 = selectedShape.width + selectedShape.startX;
+            const y2 = selectedShape.height + selectedShape.startY;
+            ctx.strokeRect(event.clientX, event.clientY, x2 - event.clientX, y2 - event.clientY);
+
+            selectedShape.startX = event.clientX;
+            selectedShape.startY = event.clientY;
+            selectedShape.width = x2 - event.clientX;
+            selectedShape.height = y2 - event.clientY;
+
+          } else if (selectedShape.type == "rectangle" && BR) {
+            // console.log(selectedShape);
+            ctx.strokeStyle = selectedShape.color;
+            ctx.lineWidth = selectedShape.stroke;
+
+            ctx.strokeRect(selectedShape.startX, selectedShape.startY, event.clientX - selectedShape.startX, event.clientY - selectedShape.startY);
+
+            selectedShape.width = event.clientX - selectedShape.startX;
+            selectedShape.height = event.clientY - selectedShape.startY;
+          } else if (selectedShape.type == "rectangle" && BL) {
+
+            ctx.strokeStyle = selectedShape.color;
+            ctx.lineWidth = selectedShape.stroke;
+
+            const newWidth = event.clientX - selectedShape.startX;
+            const newHeight = selectedShape.startY + selectedShape.height - event.clientY;
+
+            selectedShape.startY = event.clientY;
+            selectedShape.width = newWidth;
+            selectedShape.height = newHeight;
+
+
+            ctx.strokeRect(selectedShape.startX, selectedShape.startY, selectedShape.width, selectedShape.height);
+
+          } else if (selectedShape.type == "rectangle" && TR) {
+
+            ctx.strokeStyle = selectedShape.color;
+            ctx.lineWidth = selectedShape.stroke;
+            const newWidth = selectedShape.startX + selectedShape.width - event.clientX;  
+            const newHeight = event.clientY - selectedShape.startY;  
+            
+            selectedShape.startX = event.clientX;
+            selectedShape.width = newWidth;
+            selectedShape.height = newHeight;
+
+            ctx.strokeRect(selectedShape.startX, selectedShape.startY, selectedShape.width, selectedShape.height);
+
+
+          }
+
         }
+
       }
     }
   };
